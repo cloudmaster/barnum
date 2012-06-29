19aed1535454f3f6dcf97dfbcf43f2995a2b8604
Cloudmaster <ute.wissmann@sachsen-anhalt.net>
2012-06-29 15:23:31 +0200

    - slightly restructured source code for including the formatter in other code

--- gen_vcard.py
+++ gen_vcard.py
@@ -4,11 +4,21 @@ import sys, random
 import gen_data
 
 try:
-    count = int (sys.argv[1])
+	count = int(sys.argv[1])
 except:
-    count = 1
+	count = 1
 
-vcard = """BEGIN:VCARD
+class Vcard:
+	def __init__(self):
+		self.name = None
+		self.adr = None
+		self.street = None
+		self.company = None
+		self.birthday = None
+		self.email = None
+
+	def printout(self):
+		vcard = """BEGIN:VCARD
 VERSION:3.0
 FN:%s
 N:%s;%s;;;
@@ -19,17 +29,19 @@ X-JABBER;TYPE=HOME:%s
 ADR;TYPE=HOME:;;%s;%s;%s;%s;USA
 END:VCARD"""
 
-for i in range (0, count):
-    name = gen_data.create_name()
-    adr = gen_data.create_city_state_zip()
-    company = gen_data.create_company_name()
+		s = vcard % (' '.join(vc.name),
+			vc.name[1], vc.name[0],
+			vc.company, vc.birthday,
+			vc.email, vc.email, vc.street,
+			vc.adr[1], vc.adr[2], vc.adr[0])
+		return s
 
-    print >>open(str(i), "w"), vcard % (' '.join(name),
-                   name[1], name[0],
-                   company,
-                   gen_data.create_birthday(),
-                   gen_data.create_email(name=name),
-                   gen_data.create_email(name=name),
-                   gen_data.create_street(),
-                   adr[1], adr[2], adr[0],
-                   )
+for i in range(0, count):
+	vc = Vcard()
+	vc.name = gen_data.create_name()
+	vc.adr = gen_data.create_city_state_zip()
+	vc.company = gen_data.create_company_name()
+	vc.birthday = gen_data.create_birthday()
+	vc.email = gen_data.create_email(name=vc.name)
+	vc.street = gen_data.create_street()
+	print vc.printout()
